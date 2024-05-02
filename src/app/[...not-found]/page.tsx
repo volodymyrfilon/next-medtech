411104b5ebd2a339e473999edb5a57d214b1523d
Volodymyr <volodymyrfilon1312@gmail.com>
2024-05-02 14:50:49 +0200

    Project setup

---
+++ src/app/[...not-found]/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import Loading from '../loading'
+
+const page: FC = () => {
+	return (
+		<div>
+			NOT FOUND page
+			<Loading />
+		</div>
+	)
+}
+
+export default page
