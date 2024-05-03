5c5d2c648fd973b537fe7fe7725a3f57a4730397
Volodymyr <volodymyrfilon1312@gmail.com>
2024-05-03 11:34:30 +0200

    fix

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
 
 export const Footer: FC = () => {
-  return <footer className="flex justify-center text-5xl">Footer</footer>;
+  return <div className="flex justify-center text-5xl">Footer</div>;
 };
