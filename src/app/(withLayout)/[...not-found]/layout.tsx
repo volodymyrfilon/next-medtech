5ce63ec4b18a69bb058d30c74fb60918c540731e
Volodymyr <volodymyrfilon1312@gmail.com>
2024-05-22 09:58:03 +0200

    fix: styles

--- src/app/(withLayout)/[...not-found]/layout.tsx
+++ src/app/(withLayout)/[...not-found]/layout.tsx
@@ -2,6 +2,10 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Не знайдено | Терапія Душі',
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 const Layout404 = ({ children }: { children: React.ReactNode }) => {
