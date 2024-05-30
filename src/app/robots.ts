ebc31925fd4d2e982b84a844026a42d52495bc54
Volodymyr <volodymyrfilon1312@gmail.com>
2024-05-30 11:25:28 +0200

    fix: meta

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,10 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_API_BASE_URL}/sitemap.xml`,
   };
 }
